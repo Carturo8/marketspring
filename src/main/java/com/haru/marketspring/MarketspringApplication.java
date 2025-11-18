c5e93582a93f0ea05d309dfa86225c9d56b68fe5
Carturo8 <caarojasbo@unal.edu.co>
2025-11-18 06:40:26 -0500

    chore: initial Spring Boot project setup with core structure and POM dependencies

---
+++ src/main/java/com/haru/marketspring/MarketspringApplication.java
@@ -0,0 +1,13 @@
+package com.haru.marketspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haru/marketspring/MarketspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haru.marketspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

95f858876d1935d3e04feb0a831bc566a17d3ad3
Carturo8 <caarojasbo@unal.edu.co>
2025-11-18 20:22:16 -0500

    chore(app): cleanup main application class
    - Removed unnecessary annotation

--- src/main/java/com/haru/marketspring/MarketspringApplication.java
+++ src/main/java/com/haru/marketspring/MarketspringApplication.java
@@ -2,10 +2,8 @@ package com.haru.marketspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class MarketspringApplication {
 
     public static void main(String[] args) {
